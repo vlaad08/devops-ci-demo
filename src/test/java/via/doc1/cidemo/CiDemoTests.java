4a85f6fa5ada1a5fee7f7463a908c10b6366bd2d
Vlad-Alexandru Nita <vlad.alexandru194@gmail.com>
2024-04-12 09:04:31 +0200

    initial commit on forked branch

--- src/test/java/via/doc1/cidemo/CiDemoTests.java
+++ src/test/java/via/doc1/cidemo/CiDemoTests.java
@@ -1,6 +1,5 @@
 package via.doc1.cidemo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
